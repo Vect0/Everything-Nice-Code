244dca8a40b8f701d4ccc82f1bceca225f037e90
Dallas <donkeylkong@yahoo.com>
2018-09-20 13:02:54 -0500

    Update C#Practice.php

--- C#Practice.php
+++ C#Practice.php
@@ -1,5 +1,9 @@
 C# Practice:
+
 -------------
+
+read Var:
+
 using System;
 namespace HelloWorldApplication {
    
@@ -20,4 +24,45 @@ namespace HelloWorldApplication {
 
 ----------------
 
+if if else:
+
+using System;
+
+namespace DecisionMaking {
+
+   class Program {
+   
+      static void Main(string[] args) {
+         
+         int x;
+         
+         Console.WriteLine("Enter num");
+         x = Convert.ToInt32(Console.ReadLine());
+         
+         
+         if (x < 44)
+         {
+             Console.WriteLine(x + " is less than 44");
+             
+             if (x < 10)
+             {
+                 Console.WriteLine(" and " + x + " is less than 10");
+                 
+             }
+         } else if ( x > 46)
+         {
+             Console.WriteLine(x + " is greater than 46");
+             if (x > 90)
+             {
+                 Console.WriteLine(x + " is greater than 90");
+             }
+         }
+         else if (x == 45)
+         {
+             Console.WriteLine(x + " is 45");
+         }
+   }
+}
+
+------------------
 
